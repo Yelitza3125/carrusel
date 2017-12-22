1f65996d45096f8a1ea20f5c4ae016a374808e57
Yelitza3125 <deyel_99@hotmail.com>
2017-12-22 13:23:33 -0500

    Estructura de carpetas

---
+++ js/app.js
@@ -0,0 +1,65 @@
+$(document).ready(loadPage);
+var target = 0;
+
+    function loadPage() {
+    var $buttons = $('.control');
+    var $previous = $('.previous');
+    var $next = $('.next');
+    
+    
+
+    $buttons.click(changeImage);
+    $previous.click(previousImage);
+    $next.click(nextImage);
+
+};
+
+
+
+    function changeImage() {
+    target = parseInt($(this).data('target'));   
+    showImage(target);
+};
+
+    function previousImage (e) {
+    e.preventDefault();
+    target = target - 1;
+    target = (target < 0) ? 5 : target;
+    showImage(target);
+    $buttons.css('background', 'blue');
+};
+
+    function nextImage (e) {
+    e.preventDefault();
+    target = target  + 1;
+    target = (target > 5) ? 0: target;
+    showImage(target);
+};
+
+    function showImage (target) {
+    var $lastSlide = $('div.active');
+    var $slide = $("div[data-slide='" + target + "']");
+    $lastSlide.removeClass('active');
+    $slide.addClass('active');
+
+    };
+
+
+    $(document).keyup(function arrow(event) {
+        var keyCode = event.which;
+        console.log(keyCode);
+     if(keyCode === 37) {      
+        target = target - 1;
+        target = (target < 0) ? 5 : target;
+        showImage(target);
+     } else if (keyCode === 39) {
+        target = target  + 1;
+        target = (target > 5) ? 0: target;
+        showImage(target);
+     }
+
+    });
+
+
+
+
